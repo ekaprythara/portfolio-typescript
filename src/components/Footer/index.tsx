3756f9772ab3ffa1e1e074870e2a8bd462e470ed
Eka Priyanthara <ekapriyanthara072@gmail.com>
2023-12-09 17:02:23 +0800

    fix: footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,11 +3,11 @@ import { BsGithub, BsTwitterX } from "react-icons/bs";
 const Footer = () => {
   return (
     <footer className="h-32 bg-[#222] text-white dark:bg-dark text-sm">
-      <div className="flex justify-between items-center w-11/12 lg:w-9/12 mx-auto h-full">
-        <h1 className="text-base hidden md:block">
+      <div className="flex flex-col justify-center items-center md:flex-row md:justify-between md:items-center gap-5 w-11/12 lg:w-9/12 mx-auto h-full">
+        <h1 className="text-base order-2 md:order-1">
           Copyright &copy; 2023. All rights reserved.
         </h1>
-        <div className="flex gap-3">
+        <div className="flex gap-3 order-1 md:order-2">
           <a href="https://www.twitter.com/ekaprythara" target="_blank">
             <BsTwitterX size={25} className="hover:text-primary duration-500" />
           </a>
